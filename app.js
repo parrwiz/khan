71997fe06f4c6d14f55fabf699f2eb566f720667
Parwiz Khan <69708717+parrwiz@users.noreply.github.com>
2021-07-16 21:24:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', hamburger() => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
 
-document.addEventListener('scroll', () => {
+document.addEventListener('scroll', document() => {
     var scroll_position = window.scrollY;
     if (scroll_position > 250) {
         header.style.backgroundColor = '#29323c';
